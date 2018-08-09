34886b3531f783e9ce56dc7caa367f16a2dfacf8
Kuba Wiszniewski <kuba.wiszniewski@pagepro.pl>
2018-08-09 14:25:29 +0200

    Added function file and test file for fizzbuzz challange

--- src/add.js
+++
@@ -1,7 +0,0 @@
-export const add = (a, b) => {
-  if (!a || !b) {
-    throw new Error('Two numbers should be defined!')
-  } else {
-    return a + b
-  }
-}
---
+++ src/fizzbuzz.js
@@ -0,0 +1,8 @@
+// export const add = (a, b) => {
+//   if (!a || !b) {
+//     throw new Error('Two numbers should be defined!')
+//   } else {
+//     return a + b
+//   }
+// }
+export const fizzbuzz = parameter => {}
