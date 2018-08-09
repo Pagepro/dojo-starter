adfd22454c4aef9ae206d746814bcc6e4e4e115e
Jakub <jakub.dakowicz@pagepro.pl>
2018-08-09 14:01:32 +0200

    readme

---
+++ src/add.test.js
@@ -0,0 +1,21 @@
+// import {add} from './add'
+
+describe('name of test case', () => {
+  it('unit test case', () => {
+    expect(true).toBe(true)
+  })
+})
+
+// describe('add()', () => {
+//   it('adds 2+3 to be 5', () => {
+//     expect(add(2, 3)).toBe(5)
+//   })
+
+//   it('should return a number', () => {
+//     expect(add('a', 'b')).toBe('ab')
+//   })
+
+//   it('throws an error when passed undefined as a parameter', () => {
+//     expect(() => add(1)).toThrowError('Two numbers should be defined!')
+//   })
+// })
